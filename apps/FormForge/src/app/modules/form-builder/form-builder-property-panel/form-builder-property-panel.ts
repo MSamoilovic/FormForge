8a69fdccd1fae53d387820d5eacec0659d194165
Marko Samoilovic <msamoilovic@gmail.com>
2025-09-02 20:31:27 +0200

    Changes to imports on property panel

--- apps/FormForge/src/app/modules/form-builder/form-builder-property-panel/form-builder-property-panel.ts
+++ apps/FormForge/src/app/modules/form-builder/form-builder-property-panel/form-builder-property-panel.ts
@@ -1,11 +1,6 @@
 import { Component, effect, inject, input, output } from '@angular/core';
 import { CanvasField } from '@form-forge/models';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-builder-property-panel',
@@ -49,5 +44,25 @@ export class FormBuilderPropertyPanel {
     this.propertiesForm.valueChanges.subscribe((value) => {
       this.fieldChanged.emit(value);
     });
+  }
+
+  get optionsFormArray(): FormArray<FormControl<string>> {
+    return this.propertiesForm.get('options') as FormArray;
+  }
+
+  addOption(value = ''): void {
+    const options = this.propertiesForm.get('options') as FormArray;
+    options.push(this.fb.control(value));
+  }
+
+  removeOption(index: number): void {
+    const options = this.propertiesForm.get('options') as FormArray;
+    options.removeAt(index);
+  }
+
+  private setOptions(options: string[]): void {
+    const optionsArray = this.propertiesForm.get('options') as FormArray;
+    optionsArray.clear();
+    options.forEach((option) => this.addOption(option));
   }
 }
