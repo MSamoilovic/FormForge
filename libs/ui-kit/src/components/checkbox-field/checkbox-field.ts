1cf23c966baf201a4e24031fb53f082ebf07c135
Marko Samoilovic <msamoilovic@gmail.com>
2025-09-11 10:59:51 +0200

    Started work on checkbox field lib component

--- libs/ui-kit/src/components/checkbox-field/checkbox-field.ts
+++ libs/ui-kit/src/components/checkbox-field/checkbox-field.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-checkbox-field',
-  imports: [],
-  templateUrl: './checkbox-field.html',
-  styleUrl: './checkbox-field.css',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './checkbox-field.html',
+  styleUrls: ['./checkbox-field.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxField),
+      multi: true,
+    },
+  ],
 })
-export class CheckboxField {}
+export class CheckboxField implements ControlValueAccessor {
+  label = input<string>('');
+  formControl = input.required<FormControl>();
+
+  // Sva CVA logika ostaje POTPUNO ISTA.
+  writeValue(value: boolean | null): void {
+    this.formControl().setValue(value, { emitEvent: false });
+  }
+
+  registerOnChange(fn: (value: boolean | null) => void): void {
+    this.formControl().valueChanges.subscribe(fn);
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.formControl().statusChanges.subscribe(() => fn());
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    return isDisabled
+      ? this.formControl().disable()
+      : this.formControl().enable();
+  }
+}
