51b2cfd7f0cd68a750cb02ed1da121708e3ee2de
Marko Samoilovic <msamoilovic@gmail.com>
2025-09-19 14:40:43 +0200

    Refactor Form-Builder to use service

--- apps/FormForge/src/app/modules/form-builder/form-builder.component.ts
+++ apps/FormForge/src/app/modules/form-builder/form-builder.component.ts
@@ -1,18 +1,19 @@
-import { Component, computed, effect, inject, OnInit, signal, Type } from '@angular/core';
+import { Component, effect, inject, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
-import { FormBuilder, FormControl, FormGroup, FormsModule } from '@angular/forms';
-import { AvailableField, FieldOption, FieldType, FormField, FormRule, FormSchema } from '@form-forge/models';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import { AvailableField, FieldType, FormField } from '@form-forge/models';
 import { CheckboxField, DateField, RadioField, SelectorField, TextField } from '@form-forge/ui-kit';
-import { FormBuilderSidebar } from './form-builder-sidebar/form-builder-sidebar';
-import { FormBuilderCanvas } from './form-builder-canvas/form-builder-canvas';
-import { FormBuilderPropertyPanel } from './form-builder-property-panel/form-builder-property-panel';
+import { FormBuilderSidebar } from './components/form-builder-sidebar/form-builder-sidebar';
+import { FormBuilderCanvas } from './components/form-builder-canvas/form-builder-canvas';
+import { FormBuilderPropertyPanel } from './components/form-builder-property-panel/form-builder-property-panel';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ApiService } from '../core/services/api.service';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilderService } from './services/form-builder.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
@@ -29,11 +30,12 @@ import { ActivatedRoute, Router } from '@angular/router';
     MatIconModule,
     MatButton,
     MatSnackBarModule,
+    MatProgressSpinner,
     FormBuilderPropertyPanel,
   ],
-  providers: [ApiService, HttpClient],
+  providers: [ApiService, HttpClient, FormBuilderService],
 })
-export class FormBuilderComponent implements OnInit {
+export class FormBuilderComponent {
   fields: FieldType[] = [
     FieldType.Text,
     FieldType.Number,
@@ -56,22 +58,7 @@ export class FormBuilderComponent implements OnInit {
     { type: FieldType.Date, label: 'Date', icon: '' },
   ];
 
-  private fb = inject(FormBuilder);
-  private apiService = inject(ApiService);
-  private snackBar = inject(MatSnackBar);
-  private route = inject(ActivatedRoute);
-  private router = inject(Router);
-
-  canvasFields = signal<FormField[]>([]);
-  selectedField = signal<FormField | null>(null);
-  isLoading = signal<boolean>(false);
-  isEditMode = signal<boolean>(false);
-
-  private formIdToEdit = signal<number | null>(null);
-
-  pageTitle = computed(() =>
-    this.isEditMode() ? 'Edit Form' : 'Create New Form'
-  );
+  formBuilderService = inject(FormBuilderService);
 
   form = new FormGroup({});
 
@@ -86,7 +73,7 @@ export class FormBuilderComponent implements OnInit {
 
   constructor() {
     effect(() => {
-      const fields = this.canvasFields();
+      const fields = this.formBuilderService.fields();
       const currentControlIds = Object.keys(this.form.controls);
       const fieldIds = fields.map((f) => f.id);
 
@@ -96,162 +83,23 @@ export class FormBuilderComponent implements OnInit {
 
       fields
         .filter((field) => !currentControlIds.includes(field.id))
-        .forEach((field) => {
-          this.form.addControl(field.id, this.createControl(field));
-        });
+        .forEach((field) => this.form.addControl(field.id, new FormControl()));
     });
   }
 
-  ngOnInit() {
-    const idParam = this.route.snapshot.paramMap.get('id');
-    if (idParam) {
-      const formId = +idParam;
-      this.isEditMode.set(true);
-      this.formIdToEdit.set(formId);
-      this.loadFormForEdit(formId);
-    }
+  onDrop(event: CdkDragDrop<any[]>): void {
+    this.formBuilderService.dropField(event);
   }
 
-  loadFormForEdit(id: number): void {
-    this.isLoading.set(true);
-    this.apiService.getForm(id).subscribe({
-      next: (formSchema) => {
-        const fieldsWithRules = formSchema.fields.map((field) => {
-          const associatedRules =
-            formSchema.rules?.filter((rule) =>
-              rule.conditions.some(
-                (c) => 'fieldId' in c && c.fieldId === field.id
-              )
-            ) || [];
-          return { ...field, rules: associatedRules };
-        });
-
-        this.canvasFields.set(fieldsWithRules);
-        this.isLoading.set(false);
-      },
-      error: (err) => {
-        console.error('Greška pri učitavanju forme za izmenu:', err);
-        this.isLoading.set(false);
-        this.snackBar.open(`Form with ID ${id} not found.`, 'Error', {
-          duration: 5000,
-        });
-        this.router.navigate(['/dashboard']); // Vraćamo korisnika na dashboard
-      },
-    });
-  }
-
-  onDrop(event: CdkDragDrop<any[]>) {
-    const fieldType: FieldType = event.item.data;
-
-    const newField: FormField = {
-      id: crypto.randomUUID(),
-      type: fieldType,
-      label: `${fieldType} field`,
-      placeholder: '',
-      required: false,
-      options:
-        fieldType === FieldType.Select || fieldType === FieldType.Radio
-          ? [{ label: 'Option1', value: 'option1' }]
-          : [],
-    };
-
-    this.canvasFields.update((currentFields) => [...currentFields, newField]);
-    this.selectField(newField);
-  }
-
-  selectField(field: FormField) {
-    this.selectedField.set(field);
-  }
-
-  addOption() {
-    const selected = this.selectedField();
-
-    if (!selected || !selected.options) {
-      return;
-    }
-
-    const options = selected.options;
-
-    const newOption: FieldOption = {
-      label: `Option ${options.length + 1}`,
-      value: `option${options.length + 1}`,
-    };
-
-    const newOptions = [...options, newOption];
-    this.onFieldChange({ options: newOptions });
-  }
-
-  removeOption(index: number) {
-    const selected = this.selectedField();
-
-    if (!selected || !selected.options) {
-      return;
-    }
-
-    const newOptions = selected.options.filter((_, i) => i !== index);
-    this.onFieldChange({ options: newOptions });
+  onSelectField(field: FormField): void {
+    this.formBuilderService.selectField(field);
   }
 
   onFieldChange(updatedValues: Partial<FormField>): void {
-    const currentSelected = this.selectedField();
-    if (currentSelected) {
-      const updatedField = { ...currentSelected, ...updatedValues };
-
-      this.selectedField.set(updatedField);
-
-      this.canvasFields.update((fields) =>
-        fields.map((f) => (f.id === updatedField.id ? updatedField : f))
-      );
-    }
-  }
-
-  private createControl(field: FormField): FormControl {
-    return this.fb.control('');
+    this.formBuilderService.updateField(updatedValues);
   }
 
   saveForm(): void {
-    const currentFields = this.canvasFields();
-    if (currentFields.length === 0) {
-      this.snackBar.open('Cannot save an empty form.', 'Close', {
-        duration: 3000,
-      });
-      return;
-    }
-
-    const allRules: FormRule[] = currentFields.flatMap(
-      (field) => field.rules || []
-    );
-    const cleanFields: FormField[] = currentFields.map((field) => {
-      const { rules, ...fieldWithoutRules } = field;
-      return fieldWithoutRules;
-    });
-
-    const formSchema: FormSchema = {
-      id: '1',
-      name: 'My New Form',
-      description: 'A dynamically created form.',
-      fields: cleanFields,
-      rules: allRules,
-    };
-
-    this.apiService.createForm(formSchema).subscribe({
-      next: (savedForm) => {
-        console.log(
-          'Forma je uspešno sačuvana! Odgovor sa servera:',
-          savedForm
-        );
-        this.snackBar.open(`Form "${savedForm.name}" has been saved!`, 'OK', {
-          duration: 3000,
-          panelClass: 'success-snackbar',
-        });
-      },
-      error: (err) => {
-        console.error('Došlo je do greške pri čuvanju forme:', err);
-        this.snackBar.open('Error saving form. Please try again.', 'Error', {
-          duration: 5000,
-          panelClass: 'error-snackbar',
-        });
-      },
-    });
+    this.formBuilderService.saveForm();
   }
 }
